b1629efb62e782507748a34f3aac208426e83968
AsierAsoreyPrado21 <aasoreyp@fpcoruna.afundacion.org>
2023-04-27 12:21:42 +0200

    Calendario Listo

--- app/app/src/main/java/com/appf1/NavDrawer/CalendarioFragment.java
+++ app/app/src/main/java/com/appf1/NavDrawer/CalendarioFragment.java
@@ -13,11 +13,8 @@ import androidx.fragment.app.Fragment;
 
 import com.appf1.R;
 
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Locale;
-import java.util.Random;
 
 public class CalendarioFragment extends Fragment {
 
@@ -27,7 +24,7 @@ public class CalendarioFragment extends Fragment {
     private String mParam1;
     private String mParam2;
     private CalendarView calendarView;
-    private ArrayList<Long> eventos = new ArrayList<Long>();
+    private ArrayList<Evento> eventos = new ArrayList<Evento>();
 
     public CalendarioFragment() {
         // Required empty public constructor
@@ -58,7 +55,6 @@ public class CalendarioFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_calendario, container, false);
         calendarView = view.findViewById(R.id.calendarView);
-
         // Establecer listener para cuando se selecciona una fecha
         calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
@@ -68,111 +64,145 @@ public class CalendarioFragment extends Fragment {
                 calendar.set(year, month, dayOfMonth);
                 // Agregar fechas de eventos a la lista de eventos
                 Calendar evento1 = Calendar.getInstance();
-                evento1.set(2023, 2, 5); // Baréin
-                eventos.add(evento1.getTimeInMillis());
+                evento1.set(2023, 2, 5);
+                eventos.add(new Evento(evento1, "Gran Premio de Baréin"));
 
                 Calendar evento2 = Calendar.getInstance();
-                evento2.set(2023, 2, 19); // Arabia Saudita
-                eventos.add(evento2.getTimeInMillis());
+                evento2.set(2023, 2, 19);
+                eventos.add(new Evento(evento2, "Gran Premio de Arabia Saudita"));
 
                 Calendar evento3 = Calendar.getInstance();
-                evento3.set(2023, 3, 2); // Australia
-                eventos.add(evento3.getTimeInMillis());
+                evento3.set(2023, 3, 2);
+                eventos.add(new Evento(evento3, "Gran Premio de Australia"));
 
                 Calendar evento4 = Calendar.getInstance();
-                evento4.set(2023, 3, 30); //Azerbaiyán
-                eventos.add(evento4.getTimeInMillis());
+                evento4.set(2023, 3, 30);
+                eventos.add(new Evento(evento4, "Gran Premio de Azerbaiyán"));
 
                 Calendar evento5 = Calendar.getInstance();
-                evento5.set(2023, 4, 7); //Miami
-                eventos.add(evento5.getTimeInMillis());
+                evento5.set(2023, 4, 7);
+                eventos.add(new Evento(evento5, "Gran Premio de Miami"));
 
                 Calendar evento6 = Calendar.getInstance();
-                evento6.set(2023, 4, 21); //Emilia-Romaña
-                eventos.add(evento6.getTimeInMillis());
+                evento6.set(2023, 4, 21);
+                eventos.add(new Evento(evento6, "Gran Premio de Emilia-Romaña"));
 
                 Calendar evento7 = Calendar.getInstance();
-                evento7.set(2023, 4, 28); //Monaco
-                eventos.add(evento7.getTimeInMillis());
+                evento7.set(2023, 4, 28);
+                eventos.add(new Evento(evento7, "Gran Premio de Mónaco"));
 
                 Calendar evento8 = Calendar.getInstance();
-                evento8.set(2023, 5, 4); //Spain
-                eventos.add(evento8.getTimeInMillis());
+                evento8.set(2023, 5, 4);
+                eventos.add(new Evento(evento8, "Gran Premio de España"));
 
                 Calendar evento9 = Calendar.getInstance();
-                evento9.set(2023, 5, 18); //Canada
-                eventos.add(evento9.getTimeInMillis());
+                evento9.set(2023, 5, 18);
+                eventos.add(new Evento(evento9, "Gran Premio de Canadá"));
 
                 Calendar evento10 = Calendar.getInstance();
-                evento10.set(2023, 6, 2); //Austria
-                eventos.add(evento10.getTimeInMillis());
+                evento10.set(2023, 6, 2);
+                eventos.add(new Evento(evento10, "Gran Premio de Austria"));
 
                 Calendar evento11 = Calendar.getInstance();
-                evento11.set(2023, 6, 9); //Gran Bretaña
-                eventos.add(evento11.getTimeInMillis());
+                evento11.set(2023, 6, 9);
+                eventos.add(new Evento(evento11, "Gran Premio de Gran Bretaña"));
 
                 Calendar evento12 = Calendar.getInstance();
-                evento12.set(2023, 6, 23); //Hungria
-                eventos.add(evento12.getTimeInMillis());
+                evento12.set(2023, 6, 23);
+                eventos.add(new Evento(evento12, "Gran Premio de Hungría"));
 
                 Calendar evento13 = Calendar.getInstance();
-                evento13.set(2023, 6, 30); //Belgica
-                eventos.add(evento13.getTimeInMillis());
+                evento13.set(2023, 6, 30);
+                eventos.add(new Evento(evento13, "Gran Premio de Bélgica"));
 
                 Calendar evento14 = Calendar.getInstance();
-                evento14.set(2023, 7, 27); //Paises Bajos
-                eventos.add(evento14.getTimeInMillis());
+                evento14.set(2023, 7, 27);
+                eventos.add(new Evento(evento14, "Gran Premio de Países Bajos"));
 
                 Calendar evento15 = Calendar.getInstance();
-                evento15.set(2023, 8, 3); //Italia
-                eventos.add(evento15.getTimeInMillis());
+                evento15.set(2023, 8, 3);
+                eventos.add(new Evento(evento15, "Gran Premio de Italia"));
 
                 Calendar evento16 = Calendar.getInstance();
-                evento16.set(2023, 8, 17); //Singapur
-                eventos.add(evento16.getTimeInMillis());
+                evento16.set(2023, 8, 17);
+                eventos.add(new Evento(evento16, "Gran Premio de Singapur"));
 
                 Calendar evento17 = Calendar.getInstance();
-                evento17.set(2023, 8, 24); //Japon
-                eventos.add(evento17.getTimeInMillis());
+                evento17.set(2023, 8, 24);
+                eventos.add(new Evento(evento17, "Gran Premio de Japón"));
 
                 Calendar evento18 = Calendar.getInstance();
                 evento18.set(2023, 9, 8); //Catar
-                eventos.add(evento18.getTimeInMillis());
+                eventos.add(new Evento(evento18, "Gran Premio de Catar"));
 
                 Calendar evento19 = Calendar.getInstance();
-                evento19.set(2023, 9, 22); //EEUU
-                eventos.add(evento19.getTimeInMillis());
+                evento19.set(2023, 9, 22);
+                eventos.add(new Evento(evento19, "Gran Premio de EEUU"));
 
                 Calendar evento20 = Calendar.getInstance();
-                evento20.set(2023, 9, 29); //Mexico
-                eventos.add(evento20.getTimeInMillis());
+                evento20.set(2023, 9, 29);
+                eventos.add(new Evento(evento20, "Gran Premio de México"));
 
                 Calendar evento21 = Calendar.getInstance();
-                evento21.set(2023, 10, 5); //Brasil
-                eventos.add(evento21.getTimeInMillis());
+                evento21.set(2023, 10, 5);
+                eventos.add(new Evento(evento21, "Gran Premio de Brasil"));
 
                 Calendar evento22 = Calendar.getInstance();
-                evento22.set(2023, 10, 18); //Las vegas
-                eventos.add(evento22.getTimeInMillis());
+                evento22.set(2023, 10, 18);
+                eventos.add(new Evento(evento22, "Gran Premio de Las Vegas"));
 
                 Calendar evento23 = Calendar.getInstance();
-                evento23.set(2023, 10, 26); //Abu Dabi
-                eventos.add(evento23.getTimeInMillis());
-
-
+                evento23.set(2023, 10, 26);
+                eventos.add(new Evento(evento23, "Gran Premio deAbu Dabi"));
                 // Verificar si la fecha seleccionada está en la lista de eventos
                 String selectedDate = dayOfMonth + "/" + (month + 1) + "/" + year;
-                if (eventos.contains(calendar.getTimeInMillis())) {
-                    // Mostrar mensaje del evento correspondiente
-                    Toast.makeText(getActivity(), "Hay una carrera programada para el:" +selectedDate, Toast.LENGTH_SHORT).show();
+                Evento eventoSeleccionado = null;
+                for (Evento evento : eventos) {
+                    if (evento.getFecha().get(Calendar.YEAR) == year
+                            && evento.getFecha().get(Calendar.MONTH) == month
+                            && evento.getFecha().get(Calendar.DAY_OF_MONTH) == dayOfMonth) {
+                        eventoSeleccionado = evento;
+                        break;
+                    }
+                }
+                if (eventoSeleccionado != null) {
+                    // Mostrar nombre del evento correspondiente
+                    Toast.makeText(getActivity(), "Hay una carrera programada para el " + selectedDate + ": " + eventoSeleccionado.getNombre(), Toast.LENGTH_LONG).show();
                 } else {
                     // Mostrar mensaje de fecha seleccionada
-                    Toast.makeText(getActivity(), "No hay ninguna carrera programada para el: " + selectedDate, Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getActivity(), "No hay ninguna carrera programada para el " + selectedDate, Toast.LENGTH_LONG).show();
                 }
             }
+
         });
 
         return view;
+    }
+    //clase evento
+    public class Evento {
+        private Calendar fecha;
+        private String nombre;
+
+        public Evento(Calendar fecha, String nombre) {
+            this.fecha = fecha;
+            this.nombre = nombre;
+        }
+
+        public Calendar getFecha() {
+            return fecha;
+        }
+
+        public void setFecha(Calendar fecha) {
+            this.fecha = fecha;
+        }
+
+        public String getNombre() {
+            return nombre;
+        }
+
+        public void setNombre(String nombre) {
+            this.nombre = nombre;
+        }
     }
 }
 
